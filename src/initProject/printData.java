300a83dbb851a9c1797da92ece0618ef83b7acb7
Biji Jacob <bijijacob@icloud.com>
2023-11-04 22:09:17 +0530

    added print data

---
+++ src/initProject/printData.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package initProject;
+
+/**
+ * 
+ */
+public class printData {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Biji Jacob");
+	}
+
+}
